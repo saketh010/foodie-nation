053b2350c3fab3a4f25bf1ceed1a95b7451512de
Saketh <sakethkanuri10@gmail.com>
2024-06-24 12:00:05 +0300

    first

---
+++ foodie-nation-client/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_crar9-4GtBRbCXmX28TlaAgG5681NH4",
+  authDomain: "foodie-nation-client-66e34.firebaseapp.com",
+  projectId: "foodie-nation-client-66e34",
+  storageBucket: "foodie-nation-client-66e34.appspot.com",
+  messagingSenderId: "89927320716",
+  appId: "1:89927320716:web:521f78867632e3f13d27a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
